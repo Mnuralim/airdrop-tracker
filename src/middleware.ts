73277441fe1f6ca93c8654502118a120747a8425
mnuralim <shadownur345.com>
2024-09-21 18:37:51 +0800

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,28 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { auth } from './auth'
+import { RateLimiterMemory } from 'rate-limiter-flexible'
 
 export async function middleware(req: NextRequest) {
   const session = await auth()
   const baseUrl = req.nextUrl.origin
+  const rateLimiter = new RateLimiterMemory({
+    points: 5,
+    duration: 60,
+  })
 
-  if (!session) {
-    return NextResponse.redirect(`${baseUrl}/api/auth/signin`)
+  const ip = req.ip || req.headers.get('x-forwarded-for') || '127.0.0.1'
+
+  try {
+    if (!session) {
+      return NextResponse.redirect(`${baseUrl}/api/auth/signin`)
+    }
+
+    await rateLimiter.consume(ip)
+    return NextResponse.next()
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (err) {
+    return NextResponse.json({ error: 'Too many requests' }, { status: 429 })
   }
-
-  return NextResponse.next()
 }
 
 export const config = {
