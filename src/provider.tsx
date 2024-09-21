d33873ef1e174ed1f8b3fbea1a46b29659de4491
mnuralim <shadownur345.com>
2024-09-21 18:41:58 +0800

    updare

--- src/provider.tsx
+++ src/provider.tsx
@@ -1,10 +1,26 @@
 'use client'
 import { SessionProvider } from 'next-auth/react'
+import { AppProgressBar } from 'next-nprogress-bar'
 
 interface ProvidersProps {
   children: React.ReactNode
 }
 
 export default function Providers({ children }: ProvidersProps) {
-  return <SessionProvider>{children}</SessionProvider>
+  return (
+    <SessionProvider>
+      <AppProgressBar
+        height="6px"
+        color="
+        #3b82f6"
+        options={{
+          showSpinner: false,
+          easing: 'ease',
+          speed: 1500,
+        }}
+        shallowRouting
+      />
+      {children}
+    </SessionProvider>
+  )
 }
